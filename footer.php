0a101e59e3188dd98624069922114a27f4925764
dnpro <dtp@dnp.ro>
2015-03-19 17:05:19 +0200

    cleaned up

--- footer.php
+++ footer.php
@@ -10,13 +10,7 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'bushido' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'bushido' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'bushido' ), 'bushido', '<a href="http://wearska.com" rel="designer">Ionut Achim</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer id="footer"></footer><!-- #footer -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
