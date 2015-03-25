81d1658d7e71fb8ff9edaefbcead7dafd3230403
dnpro <dtp@dnp.ro>
2015-03-25 16:08:56 +0200

    buttons work

--- frontpage.php
+++ frontpage.php
@@ -13,23 +13,40 @@ get_header(); ?>
     <div class="row">
         <div class="card" style="background-color: rgb(108, 171, 218);">
             <div class="card-image">
-                <img src="http://localhost/wp-content/uploads/2015/03/lovita2_0002_Layer-Comp-3-1024x1024-300x300.jpg" alt="">
+                <img src="http://isabelarodrigues.com/wp-content/uploads/2015/01/lovita2_0002_Layer-Comp-3-960x960.jpg" alt="">
             </div>
             <div class="card-overlay" style="background-color: rgb(108, 171, 218);"></div>
             <div class="card-info">
                 <span class="card-title">El Dulce de Loco</span>
             </div>
-            <div class="card-action floating-action right">
-                    <img class="responsive-img" src="http://localhost/wp-content/uploads/2015/03/play.svg" href="http://localhost/portfolio-item/el-dulce-de-loco/">
-                </div>
+            <div class="card-action floating-action-button right">
+                <svg viewBox="0 0 32 32">
+                    <g filter="">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#4d1de92ded6458f1a7587b8f4e20a644"></use>
+                    </g>
+                </svg>
+            </div>
         </div>
     </div>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+<div class="card-action floating-action-button" style="top:40%;"></div>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
     <div class="row"></div>
     <button>Agree</button>
-    <button>Disagree</button>
-    <button class="raised">Accept</button>
-    <button class="colored pull-left">Ok</button>
-    <button class="colored, pull-right">Cancel</button>
+    <button class="colored">Disagree</button>
+    <button class="">Accept</button>
+    <button class="flat colored">Ok</button>
+    <button class="flat">Cancel</button>
     </br>
 </div>
 
